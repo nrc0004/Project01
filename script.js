2b545e14177ea89d7a378f2e769afd8db0ebd245
Natasha Clark <Natasharclark@gmail.com>
2017-01-31 12:07:20 -0500

    js is up and working, starting on game logic

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello world")
+
+
+var button = $(".middle");
+var pop = $("#card1");
+var science = $("#card2");
+var lit = $("#card3");
+var poli = $("#card4");
+var his = $("#card5");
+var catagories= [
+  "Pop Culture", "Science", "Literature", "Politics", "History" ];
+var random= catagories[Math.floor(Math.random()*catagories.length)];
+
+button.on("click", function(){
+
+var random= catagories[Math.floor(Math.random()*catagories.length)];
+console.log (random);
+})
