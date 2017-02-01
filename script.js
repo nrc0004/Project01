5e53eb538ab8199471a3f41684b2af34cb89a63c
Natasha Clark <Natasharclark@gmail.com>
2017-02-01 00:23:06 -0500

    JS WORKS-- need to change data

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("hello world");
+
+var questions = [ {
+  "question": "what is a cat?",
+  "option1":"Animal",
+  "option2": "Fish",
+  "option3":"bird",
+  "option4":"fruit",
+  "answer":"2"
+}, {
+  "question": "what is a dog?",
+  "option1":"Animal",
+  "option2": "Fish",
+  "option3":"bird",
+  "option4":"fruit",
+  "answer":"3"
+}, {
+  "question": "what is a bird?",
+  "option1":"Animal",
+  "option2": "Fish",
+  "option3":"bird",
+  "option4":"fruit",
+  "answer":"1"
+}]
